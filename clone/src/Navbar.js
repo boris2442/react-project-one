ed7f3c8bc3c82045792150352cc16ccb57913566
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-01 17:04:35 +0200

    text

--- clone/src/Navbar.js
+++ clone/src/Navbar.js
@@ -1,6 +1,6 @@
 // import {Link} from 'react-router-dom';
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <header className="home">
       <div className="">
@@ -8,27 +8,37 @@ const navbar = () => {
       </div>
       <nav>
         <ul>
-        <li>
-            <a className=""  href="/home">Home</a>
+          <li>
+            <a className="" href="/home">
+              Home
+            </a>
           </li>
           <li>
-            <a className="" href="#one">A la une</a>
+            <a className="" href="#one">
+              A la une
+            </a>
           </li>
           <li>
-            <a className="boutique" href="#boutique">Boutique</a>
+            <a className="boutique" href="#boutique">
+              Boutique
+            </a>
           </li>
-          
+
           <li>
-            <a className="arrivage" href="#arrivage">Arrivages</a>
+            <a className="arrivage" href="#arrivage">
+              Arrivages
+            </a>
           </li>
           <li>
-            <a className="contact" href="#contact">Newsletter</a>
+            <a className="contact" href="#contact">
+              Newsletter
+            </a>
           </li>
         </ul>
-        
       </nav>
+      <div className="">{/* <i className="fa-solid fa-bars"></i> */}</div>
     </header>
   );
 };
 
-export default navbar;
+export default Navbar;
